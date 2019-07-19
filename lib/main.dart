74ad775d8d05fe54050a494dd0873d10b70a5b94
mcndoungerii <allenmcndounger@gmail.com>
2019-07-19 11:17:07 +0300

    Adding a Transactiona Model $ Transaction Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      home: MyHomePage()
-    );
+    return MaterialApp(title: 'Flutter App', home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: '1', title: 'Crops', amount: 20000, date: DateTime.now()),
+    Transaction(id: '2', title: 'Products', amount: 30000, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('CHARTS!'),
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHARTS!'),
+              ),
             ),
-          ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-          )
-        ],
-      )
-    );
+            Card(
+              color: Colors.red,
+              child: Text('LIST OF TX'),
+            )
+          ],
+        ));
   }
 }
