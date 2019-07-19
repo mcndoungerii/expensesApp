52218b66e8e2d7fae7f3297576b4f4f17e441894
mcndoungerii <allenmcndounger@gmail.com>
2019-07-19 10:13:37 +0300

    Understanding Column Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -29,6 +31,7 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Card(
+            color: Colors.red,
             child: Text('LIST OF TX'),
           )
         ],
