410d02bccc3322aa88ebf43c96fc805bd5b38b11
mcndoungerii <allenmcndounger@gmail.com>
2019-07-20 12:17:42 +0300

    More Styling..!

--- lib/main.dart
+++ lib/main.dart
@@ -64,9 +64,19 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(transaction.title),
-                        Text(transaction.date.toString())
+                        Text(
+                          transaction.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          transaction.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        )
                       ],
                     )
                   ],
