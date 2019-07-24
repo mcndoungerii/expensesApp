32888666e7b0f0d6ac2127a7ca692ea676d21660
mcndoungerii <allenmcndounger@gmail.com>
2019-07-24 09:43:49 +0300

    Further Input & OUtput Configuration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,20 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      titleController.text,
+      double.parse(amountController.text),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,6 +34,8 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
               //onChanged: (val) {
               //titleInput = val;
               //},
@@ -27,22 +43,14 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
               //onChanged: (val) => amountInput = val,
             ),
             FlatButton(
-              child: Text('Add Transaction'),
-              textColor: Colors.purple,
-              onPressed: () {
-                //print(titleInput);
-                //  print(amountInput);
-                //print(titleController.text);
-                //print(amountController.text);
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
-            ),
+                child: Text('Add Transaction'),
+                textColor: Colors.purple,
+                onPressed: () => submitData()),
           ],
         ),
       ),
