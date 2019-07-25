b1175701294002e2b1923d05d773fb0bdcfed3e9
mcndoungerii <allenmcndounger@gmail.com>
2019-07-25 16:39:43 +0300

    Deriving recent Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:expenses_app/widgets/transaction_list.dart';
-
-import './widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
+import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
 import './models/transaction.dart';
 
 void main() => runApp(MyApp());
@@ -18,10 +17,9 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Quicksand',
           textTheme: ThemeData.light().textTheme.copyWith(
                 title: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'OpenSans',
-                  fontSize: 18
-                ),
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'OpenSans',
+                    fontSize: 18),
               ),
           appBarTheme: AppBarTheme(
               textTheme: ThemeData.light().textTheme.copyWith(
@@ -46,19 +44,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-      //Transaction(
-      //  id: 't1',
-      //  title: 'New Shoes',
-      //  amount: 69.99,
-      //  date: DateTime.now(),
-      //),
-      //Transaction(
-        //id: 't2',
-        //title: 'Weekly Groceries',
-        //amount: 16.53,
-        //date: DateTime.now(),
-      //),
+    //Transaction(
+    //  id: 't1',
+    //  title: 'New Shoes',
+    //  amount: 69.99,
+    //  date: DateTime.now(),
+    //),
+    //Transaction(
+    //id: 't2',
+    //title: 'Weekly Groceries',
+    //amount: 16.53,
+    //date: DateTime.now(),
+    //),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) { //Helps to run a function in a list & if the f(x) return true It returns a new List
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -105,14 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions)
           ],
         ),
---
+++ lib/widgets/chart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      var weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekDay.day &&
+            recentTransaction[i].date.month == weekDay.month &&
+            recentTransaction[i].date.year == weekDay.year) {
+          totalSum += recentTransaction[i].amount;
+        }
+      }
+
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 5,
+        margin: EdgeInsets.all(10),
+        child: Row(
+          children: groupedTransactionValues.map((data) {
+            return Text('${data['day']} + : ${data['amount']}');
+          }).toList(),
+        ));
+  }
+}
