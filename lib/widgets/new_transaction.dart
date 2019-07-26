9af2db34e56ab2cb4b7221cf28da8851fbcdadec
mcndoungerii <allenmcndounger@gmail.com>
2019-07-26 12:29:18 +0300

    Date  Selection

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -55,6 +55,22 @@ class _NewTransactionState extends State<NewTransaction> {
               onSubmitted: (_) => submitData(),
               //onChanged: (val) => amountInput = val,
             ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Text('No Date Chosen!'),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
             FlatButton(
                 child: Text('Add Transaction'),
                 textColor: Colors.purple,
